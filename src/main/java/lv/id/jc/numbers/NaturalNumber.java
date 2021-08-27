c2ca77ed2cca31ffa105f43172713e68b988ebf0
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-27 23:22:54 +0300

    Add NaturalNumberTest

--- src/main/java/lv/id/jc/numbers/NaturalNumber.java
+++ src/main/java/lv/id/jc/numbers/NaturalNumber.java
@@ -1,9 +1,16 @@
 package lv.id.jc.numbers;
 
 import java.math.BigInteger;
+import java.util.regex.Pattern;
 
 public class NaturalNumber extends BigInteger {
+    private static final Pattern NATURAL_NUMBER = Pattern.compile("[+]?(?!0+$)\\d*\\d");
+
     public NaturalNumber(String val) {
         super(val);
+    }
+
+    static boolean isNaturalNumber(final String value) {
+        return NATURAL_NUMBER.matcher(value).matches();
     }
 }
---
+++ src/test/groovy/lv/id/jc/numbers/NaturalNumberTest.groovy
@@ -0,0 +1,14 @@
+package lv.id.jc.numbers
+
+
+import spock.lang.Specification
+
+class NaturalNumberTest extends Specification {
+    def "expects that #value is a natural number"() {
+        expect:
+        NaturalNumber.isNaturalNumber(value)
+
+        where:
+        value << ["1", "+1", "01", "001", "+001", "2", "320", "+00023003210900"]
+    }
+}
