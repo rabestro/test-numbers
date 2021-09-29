e2a40d9ceb1bd3d4ab50e6d21985694a8b5754dc
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-29 22:37:33 +0300

    Update EvenTest

--- src/test/groovy/lv/id/jc/numbers/property/EvenTest.groovy
+++ src/test/groovy/lv/id/jc/numbers/property/EvenTest.groovy
@@ -12,7 +12,7 @@ class EvenTest extends Specification {
         property.test(number as long)
 
         where:
-        number << [2, 4, 14, 40, 10, 18652, 453066, 4000]
+        number << [2, 4, 8, 10, 12, 18, 22, 36]
     }
 
     def 'expects that #number is not an even number'() {
@@ -20,6 +20,6 @@ class EvenTest extends Specification {
         !property.test(number as long)
 
         where:
-        number << [1, 3, 7, 11, 19, 5221, 145, 31]
+        number << [1, 3, 7, 11, 19, 21, 31, 49]
     }
 }
